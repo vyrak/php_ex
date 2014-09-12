1427c3725db339a011c36b33419d5109596b99da
vyrak bunleang <vyrak.bunleang@gmail.com>
2014-09-11 20:14:41 -0600

    fix variables and sql statements
    - be explicit on the order of columns when inserting or updating rows
    - variable names match element name & id, consistency makes reading code
      easier to follow & maintain

--- phonebook.php
+++ phonebook.php
@@ -2,7 +2,7 @@
   <head>
     <title>Collection of Irrelevant People: The Phonebook</title>
     <script language='JavaScript'>
-      var theAction='Add';
+      var action='Add';
       function checkForm()
       {
         var areThereErrors='no';
@@ -22,31 +22,31 @@
         lastNameError=document.getElementById('LastNameError');
         lastNameError.innerHTML=null;
 
-        var theAddress=document.getElementById('theAddress');
-        theAddress.style.backgroundColor='white';
-        theAddressError=document.getElementById('theAddress');
-        theAddressError.innerHTML=null;
+        var address=document.getElementById('address');
+        address.style.backgroundColor='white';
+        addressError=document.getElementById('address');
+        addressError.innerHTML=null;
 
-        var theCity=document.getElementById('theCity');
-        theCity.style.backgroundColor='white';
-        theCityError=document.getElementById('theCity');
-        theCityError.inner.HTML=null;
+        var city=document.getElementById('city');
+        city.style.backgroundColor='white';
+        cityError=document.getElementById('city');
+        cityError.inner.HTML=null;
 
-        var theState=document.getELementById('theState');
-        theState.style.backgroundColor='white';
-        theStateError=document.getElementById('theState');
-        theStateError.inner.HTML=null;
+        var state=document.getElementById('state');
+        state.style.backgroundColor='white';
+        stateError=document.getElementById('state');
+        stateError.inner.HTML=null;
 
-        var zipCode=document.getElementById('zipCode');
-        zipCode.style.backgroundColor='white';
-        zipCodeError=document.getElementById('zipCode');
-        zipCodeError.inner.HTML=null;
+        var zip=document.getElementById('zip');
+        zip.style.backgroundColor='white';
+        zipError=document.getElementById('zip');
+        zipError.inner.HTML=null;
 
-        if(theAction === 'add' || theAction==='Update' || theState.value.length < 2)
+        if(action === 'add' || action==='Update' || state.value.length < 2)
           {
             areThereErrors='yes';
-            theState.style.backgroundColor='red';
-            theStateError.innerHTML="Enter the state in the format of XX";
+            state.style.backgroundColor='red';
+            stateError.innerHTML="Enter the state in the format of XX";
           }
         if (areThereErrors === 'yes')
           return false;
@@ -60,63 +60,63 @@
         var phoneNumber=document.getElementById('Phone');
         var firstName=document.getElementById('firstName');
         var lastName=document.getElementById('lastName');
-        var theAddress=document.getElementById('theAddress');
-        var theCity=document.getElementById('theCity');
-        var theState=document.getElementById('theState');
-        var zipCode=document.getELementById('zipCode');
+        var address=document.getElementById('address');
+        var city=document.getElementById('city');
+        var state=document.getElementById('state');
+        var zip=document.getElementById('zip');
 
         switch(Action)
         {
           case 1:
-            theSubmitButton.value='Add';
+            submitButton.value='Add';
             phoneNumber.readOnly=false;
             firstName.readOnly=false;
             lastName.readOnly=false;
-            theAddress.readOnly=false;
-            theCity.readOnly=false;
-            theState.readOnly=false;
-            zipCode.readOnly=false;
-            theAction='Add';
+            address.readOnly=false;
+            city.readOnly=false;
+            state.readOnly=false;
+            zip.readOnly=false;
+            action='Add';
             break;
 
           case 2:
-            theSubmitButton.value='Delete';
+            submitButton.value='Delete';
             phoneNumber.readOnly=false;
             firstName.readOnly=false;
             lastName.readOnly=false;
-            theAddress.readOnly=false;
-            theCity.readOnly=false;
-            theState.readOnly=false;
-            zipCode.readOnly=false;
-            theAction='Delete';
+            address.readOnly=false;
+            city.readOnly=false;
+            state.readOnly=false;
+            zip.readOnly=false;
+            action='Delete';
             break;
 
           case 3:
-            theSubmitButton.value='Edit';
+            submitButton.value='Edit';
             phoneNumber.readOnly=false;
             firstName.readOnly=false;
             lastName.readOnly=false;
-            theAddress.readOnly=false;
-            theCity.readOnly=false;
-            theState.readOnly=false;
-            zipCode.readOnly=false;
-            theAction='Edit';
+            address.readOnly=false;
+            city.readOnly=false;
+            state.readOnly=false;
+            zip.readOnly=false;
+            action='Edit';
             break;
 
           case 4:
-            theSubmitButton.value='View';
+            submitButton.value='View';
             phoneNumber.readOnly=false;
             firstName.readOnly=false;
             lastName.readOnly=false;
-            theAddress.readOnly=false;
-            theCity.readOnly=false;
-            theState.readOnly=false;
-            zipCode.readOnly=false;
-            theAction='View';
+            address.readOnly=false;
+            city.readOnly=false;
+            state.readOnly=false;
+            zip.readOnly=false;
+            action='View';
             break;
 
           default:
-            theSubmitButton.value='Error';
+            submitButton.value='Error';
             break;
         }
 
@@ -125,87 +125,79 @@
   </head>
 
   <body>
-    <div id="main">
     <?php
       if($_POST["Reset"])
       {
         $Phone = "";
         $firstName = "";
         $lastName = "";
-        $Address = "";
-        $City = "";
-        $State = "";
-        $Zip = "";
+        $address = "";
+        $city = "";
+        $state = "";
+        $zip = "";
       }
       else
       {
         echo "From form <br/><br/>\n";
         $Action=$_POST['Action'];
         $Phone=$_POST['Phone'];
-        $firstName=$POST['firstName'];
+        $firstName=$_POST['firstName'];
         $lastName=$_POST['lastName'];
-        $Address=$_POST['Address'];
-        $City=$_POST['City'];
-        $State=$_POST['State'];
-        $Zip=$_POST['Zip'];
+        $address=$_POST['address'];
+        $city=$_POST['city'];
+        $state=$_POST['state'];
+        $zip=$_POST['zip'];
         $conn=mysql_connect('localhost', 'alpha') or die(mysql_error());
         mysql_select_db("test", $conn);
 
         switch($Action)
         {
           case 'A':
-            echo "ADD \n";
-            $SQL1 = " insert into Phonebook values ( " . 
+            $SQL1 = "insert into phonebook(phone, firstName, lastName, address, city, state, zip) values (" .
                 " '$Phone', " .
                 " '$firstName', " .
                 " '$lastName', " .
-                " '$Address', " .
-                " '$City', " .
-                " '$State', " .
-                " '$Zip', " .
+                " '$address', " .
+                " '$city', " .
+                " '$state', " .
+                " '$zip'" .
                 " ) ";
-            //echo $SQL1;
             $result1 = mysql_query($SQL1, $conn) or die(mysql_error());
             echo "<br/>\n";
             break;
 
           case 'D':
-            echo "Delete\n";
             $SQL1 = "delete from Phonebook where Phone = '$Phone' ";
-            //echo $SQL1;
             $result1 = mysql_query($SQL1, $conn) or die(mysql_error());
             echo "<br/>\n";
             break;
 
           case 'U';
-            echo "Update\n";
-            $SQL1 = " update Phonebook set " .
+            $SQL1 = "update Phonebook set " .
                 " firstName = '$firstName', " .
                 " lastName = '$lastName', " .
-                " Address = '$Address', " .
-                " City = '$City', " .
-                " State = '$State', " .
-                " Zip = '$Zip', " .
+                " address = '$address', " .
+                " city = '$city', " .
+                " state = '$state', " .
+                " zip = '$zip'" .
                 " where Phone= '$Phone' ";
             $result1 = mysql_query($SQL1, $conn) or die(mysql_error());
             break;
 
           case 'L':
-            echo "Lookup\n";
             $SQL1 = " select Phone, firstname, lastName,
-                  Address, City, State, Zip" .
+                  address, city, state, zip" .
                 " from Phonebook " .
                 " where Phone = '$Phone' ";
-            echo $SQL1;
             $result1 = mysql_query($SQL1, $conn) or die(mysql_error());
             $array1 = mysql_fetch_array($result1);
             $Phone = $array1['Phone'];
             $firstName = $array1['firstName'];
             $lastName = $array1['lastName'];
-            $Address = $array1['Address'];
-            $City = $array1['City'];
-            $State = $array1['State'];
-            $Zip = $array1['Zip'];
+            $address = $array1['address'];
+            $city = $array1['city'];
+            $state = $array1['state'];
+            $zip = $array1['zip'];
             break;
 
           default:
@@ -220,7 +212,7 @@
       echo " <input type='radio' name='Action' id='Action1' value='A' checked onClick='ProtectFields(1)'> Add \n";
       echo " <input type='radio' name='Action' id='Action2' value='D' checked onClick='ProtectFields(2)'> Delete \n";
       echo " <input type='radio' name='Action' id='Action3' value='U' checked onClick='ProtectFields(3)'> Update \n";
-      echo " <input type='radio' name='Action' id='Action4' value='L' checked onClick='ProtectedFields(4)'> Lookup \n";
+      echo " <input type='radio' name='Action' id='Action4' value='L' checked onClick='ProtectFields(4)'> Lookup \n";
       echo " <br/> \n";
       echo " Phone : <input type='text' name='Phone' id='Phone' value='$Phone' size='12' maxlength='12'> \n";
       echo " <br/> \n";
@@ -230,22 +222,21 @@
       echo " First Name: <input type='text' name='firstName' id='firstName' value='$firstName' size='25' maxlength='25'> \n";
       echo " Last Name: <input type='text' name='lastName' id='lastName' value='$lastName' size='25' maxlength='25'> \n";
       echo " <br/><br/> \n";
-      echo " Address: <input type='text' name='Address' id='Address' value='$Address' size='45' maxlength='45'> \n";
+      echo " Address: <input type='text' name='address' id='address' value='$address' size='45' maxlength='45'> \n";
       echo " <br/><br/> \n";
-      echo " City: <input type='text' name='City' id='City' value='$City' size='25' maxlength='25'> \n";
+      echo " City: <input type='text' name='city' id='city' value='$city' size='25' maxlength='25'> \n";
       echo " <br/><br/> \n";
-      echo " State: <input type='text' name'State' it='State' value'$State' size='2' maxlength='2'> \n";
+      echo " State: <input type='text' name='state' id='state' value='$state' size='2' maxlength='2'> \n";
       echo " <br/><br/> \n";
       echo " <span id='StateError'> </span>\n";
       echo " <br/><br/> \n";
-      echo " Zip: <input type='text' name='Zip' id='Zip' value='$Zip' size='5' maxlength='5'> \n";
+      echo " Zip: <input type='text' name='zip' id='zip' value='$zip' size='5' maxlength='5'> \n";
       echo " <br/><br/> \n";
       echo " <span id='ZipError'></span>\n";
       echo " <br/><br/> \n";
-      echo " <input type='Submit' value='Record Values'> \n";
+      echo " <input id='SubmitButton' type='Submit' value='Record Values'> \n";
       echo " <input type='Reset' value='Reset'> \n";
       echo " </form> \n";
     ?>
-
   </body>
 </HTML>
